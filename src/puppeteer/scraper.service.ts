cbc136d50881f29989c0d1d9721b929811e5e787
Burak Zeren <opkbmz@hotmail.com>
2025-03-11 00:30:50 +0300

    Scripping done with everything

--- src/puppeteer/scraper.service.ts
+++ src/puppeteer/scraper.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer';
+import { StockData } from 'src/types/stockData';
 
 @Injectable()
 export class ScraperService {
@@ -7,19 +8,28 @@ export class ScraperService {
     console.log('Scraping foreks.com...');
 
     try {
-      const browser = await puppeteer.launch({ headless: false });
+      const browser = await puppeteer.launch({
+        headless: false,
+        defaultViewport: {
+          width: 925,
+          height: 998,
+        },
+      });
       const page = await browser.newPage();
-      await page.goto('https://www.foreks.com/borsa/', { waitUntil: 'networkidle2', timeout: 60000 });
+      await page.goto('https://www.foreks.com/borsa/', {
+        waitUntil: 'networkidle2',
+        timeout: 60000,
+      });
 
       await page.waitForSelector('.select', { timeout: 60000 });
       await page.select('.select', 'ALL');
 
       await page.waitForSelector('#definitionsTable', { timeout: 60000 });
-      
+
       await page.waitForSelector('#definitionsTable', { timeout: 60000 });
       console.log('Table loaded successfully.');
 
-      let data: string[][] = [];
+      let data: StockData[] = [];
       let pageCounter = 1;
 
       while (true) {
@@ -27,27 +37,71 @@ export class ScraperService {
 
         const currentPageData = await page.evaluate(() => {
           const rows = document.querySelectorAll('#definitionsTable tr');
-          return Array.from(rows).map(row =>
-            Array.from(row.querySelectorAll('td')).map(col => col.textContent?.trim() || '')
-          );
+
+          return Array.from(rows).map((row) => {
+            const cells: StockData = {
+              sembol:
+                row.querySelector('td:nth-child(1)')?.textContent?.trim() || '',
+              son:
+                row.querySelector('td:nth-child(2)')?.textContent?.trim() || '',
+              farkYuzde:
+                row.querySelector('td:nth-child(3)')?.textContent?.trim() || '',
+              fark:
+                row.querySelector('td:nth-child(4)')?.textContent?.trim() || '',
+              alis:
+                row.querySelector('td:nth-child(5)')?.textContent?.trim() || '',
+              satis:
+                row.querySelector('td:nth-child(6)')?.textContent?.trim() || '',
+              gYuksek:
+                row.querySelector('td:nth-child(7)')?.textContent?.trim() || '',
+              gDusuk:
+                row.querySelector('td:nth-child(8)')?.textContent?.trim() || '',
+              aOrt:
+                row.querySelector('td:nth-child(9)')?.textContent?.trim() || '',
+              adet:
+                row.querySelector('td:nth-child(10)')?.textContent?.trim() ||
+                '',
+              hacim:
+                row.querySelector('td:nth-child(11)')?.textContent?.trim() ||
+                '',
+              süre:
+                row.querySelector('td:nth-child(12)')?.textContent?.trim() ||
+                '',
+            };
+            return cells;
+          });
         });
 
-        data.push(...currentPageData); 
-        console.log(`Page ${pageCounter} data extracted. Total records: ${data.length}`);
+        data.push(...currentPageData);
+        console.log(
+          `Page ${pageCounter} data extracted. Total records: ${data.length}`,
+        );
 
-        const nextButton = await page.$('button.page-link[aria-label="Go to next page"]');
+        const nextButton = await page.$(
+          'button.page-link[aria-label="Go to next page"]',
+        );
 
         if (nextButton) {
-          const isDisabled = await page.evaluate(button => button.hasAttribute('disabled'), nextButton);
+          const isDisabled = await page.evaluate(
+            (button) => button.hasAttribute('disabled'),
+            nextButton,
+          );
 
           if (!isDisabled) {
             console.log(`Clicking next page (${pageCounter + 1})...`);
-            
-            await page.evaluate(button => button.click(), nextButton);
 
-            await page.waitForSelector('#definitionsTable tr', { timeout: 30000 });
+            await page.evaluate((button) => button.click(), nextButton);
 
-            await page.waitForResponse(response => response.url().includes('foreks.com') && response.status() === 200, { timeout: 30000 });
+            await page.waitForSelector('#definitionsTable tr', {
+              timeout: 30000,
+            });
+
+            await page.waitForResponse(
+              (response) =>
+                response.url().includes('foreks.com') &&
+                response.status() === 200,
+              { timeout: 30000 },
+            );
 
             pageCounter++;
           } else {
---
+++ src/types/stockData.ts
@@ -0,0 +1,15 @@
+export interface StockData {
+    sembol: string;
+    son: string;
+    farkYuzde: string;
+    fark: string;
+    alis: string;
+    satis: string;
+    gYuksek: string;
+    gDusuk: string;
+    aOrt: string;
+    adet: string;
+    hacim: string;
+    süre?: string;
+  }
+  
